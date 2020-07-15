38679abc97b465b5e980effc509a56d5d1979954
Ethan Hu <fendouhyz@163.com>
2020-07-15 16:23:27 +0800

    feature: add lockscript of crosschain

---
+++ ckb-contracts-rust/contracts/lockscript/src/main.rs
@@ -0,0 +1,65 @@
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+#![feature(alloc_error_handler)]
+#![feature(panic_info_message)]
+
+// Import from `core` instead of from `std` since we are in no-std mode
+use core::result::Result;
+
+// Import CKB syscalls and structures
+// https://nervosnetwork.github.io/ckb-std/riscv64imac-unknown-none-elf/doc/ckb_std/index.html
+use ckb_std::{
+    ckb_constants::Source,
+    entry,
+    default_alloc,
+    high_level::{load_script, load_cell_type_hash},
+    error::SysError,
+    ckb_types::{bytes::Bytes, prelude::*},
+};
+
+entry!(entry);
+default_alloc!();
+
+/// Program entry
+fn entry() -> i8 {
+    // Call main function and return error code
+    match main() {
+        Ok(_) => 0,
+        Err(err) => err as i8,
+    }
+}
+
+/// Error
+#[repr(i8)]
+enum Error {
+    IndexOutOfBound = 1,
+    ItemMissing,
+    LengthNotEnough,
+    Encoding,
+    // Add customized errors here...
+    TypesScriptMisMatch,
+}
+
+impl From<SysError> for Error {
+    fn from(err: SysError) -> Self {
+        use SysError::*;
+        match err {
+            IndexOutOfBound => Self::IndexOutOfBound,
+            ItemMissing => Self::ItemMissing,
+            LengthNotEnough(_) => Self::LengthNotEnough,
+            Encoding => Self::Encoding,
+            Unknown(err_code) => panic!("unexpected sys error {}", err_code),
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let script = load_script()?;
+    let args: Bytes = script.args().unpack();
+    let type_hash = load_cell_type_hash(0, Source::Input)?;
+    if &args[..] != &type_hash[..] {
+        return Err(Error::TypesScriptMisMatch);
+    }
+    Ok(())
+}
